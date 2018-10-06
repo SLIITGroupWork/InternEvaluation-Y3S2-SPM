caf7c5b683c22e1acf36606edcc60699593338ed
RavinduDharmasena <ravinduta@gmail.com>
2018-10-06 15:56:20 +0530

    Implemented Viva Controller And Viva Route

--- Backend/DBSchemas/schemas.js
+++ Backend/DBSchemas/schemas.js
@@ -119,12 +119,37 @@ const internReportSchema = new Schema({
     }]
 });
 
+const vivaSchedule = new Schema({
+   batch:{
+       type:String,
+       required:true
+   },
+   specialization:{
+       type:String,
+       required:true
+   },
+   scheduleDate:{
+       type:Date,
+       required:true
+   },
+   allocationTime:{
+       type:Number,
+       required:true
+   },
+   endDate:{
+       type:Date,
+       required:true
+   }
+});
+
 Mongoose.model('User', User);
 Mongoose.model('ActivityDummy', ActivityDummy);
 
 Mongoose.model('InternReport', internReportSchema);
 
 Mongoose.model('FormI5',FormI5);
+
+Mongoose.model('VivaSchedule',vivaSchedule);
 
 Mongoose.connect('mongodb://localhost:27017/internDB', (err) => {
     if (err) {
